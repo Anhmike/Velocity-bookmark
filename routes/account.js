899bf254aa3ba86f1fab69d83088d6468639b5a0
jberk16 <jberk16@gmail.com>
2016-07-21 18:33:13 -0400

    Update

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var profileController = require('../controllers/ProfileController')
+var bcrypt = require('bcryptjs')
 
 router.post('/:action', function(req, res, next) {
 	var action = req.params.action
@@ -27,7 +28,8 @@ router.post('/:action', function(req, res, next) {
 			}
 
 			var user = profiles[0]
-			if (req.body.password != user.password){
+			var isPasswordCorrect =  bcrypt.compareSync(req.body.password, user.password)
+			if (isPasswordCorrect == false){
 				res.json({
 					confirmation: 'fail',
 					message: 'Wrong password'
